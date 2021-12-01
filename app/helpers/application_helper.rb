48e54849b9c60de47b34158d6a80b67d0a4522d4
Arthur ALEGO <arthur.neto@al.go.leg.br>
2021-12-01 10:41:01 -0300

    Criando helper para mostrar ambiente de Produção / Teste / Desenvolvimento

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,14 @@ module ApplicationHelper
     def nome_aplicacao
         "Crypto Wallet APP"
     end
-    
+
+    def ambiente_rails 
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
 end
