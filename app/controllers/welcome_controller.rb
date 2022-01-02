5deb51528bd882328a7c583d7793bf1bc8b6d348
Arthur Mariano <thiesto@gmail.com>
2022-01-02 16:07:14 -0300

    83 - Cookies e Session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] =  "Curso de Ruby on Rails - Arthur Mariano [Cookie]"
+    session[:curso] =  "Curso de Ruby on Rails - Arthur Mariano [Session]"
     @meu_nome = params[:nome]
     @meu_curso = params[:curso]
   end
